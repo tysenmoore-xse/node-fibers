79ec741543bafda4fc6972545fe3287bb09bf23b
tmoore <tysen_moore@mentor.com>
2017-09-20 09:06:45 -0400

    - Modified code to support node packaging tools like: pkg, nexe, etc.

--- fibers.js
+++ fibers.js
@@ -9,17 +9,20 @@ if (process.fiberLib) {
 	// Look for binary for this platform
 	var modPath = path.join(__dirname, 'bin', process.platform+ '-'+ process.arch+ '-'+ process.versions.modules, 'fibers');
 	try {
-		fs.statSync(modPath+ '.node');
-	} catch (ex) {
-		// No binary!
-		console.error(
-			'## There is an issue with `node-fibers` ##\n'+
-			'`'+ modPath+ '.node` is missing.\n\n'+
-			'Try running this to fix the issue: '+ process.execPath+ ' '+ __dirname.replace(' ', '\\ ')+ '/build'
-		);
-		throw new Error('Missing binary. See message above.');
+		// Pull in fibers implementation
+		process.fiberLib = module.exports = require(modPath).Fiber;
+	} catch (err) {
+		// See if the module has even been compiled/exists
+		try {
+			fs.statSync(modPath+ '.node');
+		} catch (ex) {
+			// No binary!
+			console.error(
+				'## There is an issue with `node-fibers` ##\n'+
+				'`'+ modPath+ '.node` is missing.\n\n'+
+				'Try running this to fix the issue: '+ process.execPath+ ' '+ __dirname.replace(' ', '\\ ')+ '/build'
+			);
+			throw new Error('Missing binary. See message above.');
+		}
 	}
-
-	// Pull in fibers implementation
-	process.fiberLib = module.exports = require(modPath).Fiber;
 }
